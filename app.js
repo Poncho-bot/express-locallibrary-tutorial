177b8aec9c43e934857bc4de7ddf90dc4d440c1f
unknown <hell.faith2238@gmail.com>
2022-02-14 13:36:11 -0600

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(compression()); //Compress all routes
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Alberto:Alberto@cluster0.vibox.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Alberto:Alberto@cluster0.vibox.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
